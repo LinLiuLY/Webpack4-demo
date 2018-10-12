7ee04b5dcad36c80726c3931363181f42bbbbc92
liulin <liulin1987315@gmail.com>
2018-10-12 19:23:33 +1100

    Add source-map for prod mode.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-console.log('Hello world');
+console.log('Hello world')
 
-var array = [1,2,3];
+var array = [1, 2, 3]
 
-Array.from(array).forEach(($item) => {
- console.log($item);
+Array.from(array).forEach($item => {
+  console.log($item)
 })
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!')
+} else {
+  console.log('Looks like we are in production mode!')
+}
